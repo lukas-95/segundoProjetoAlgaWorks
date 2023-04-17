13c2e2221a54bb3679cec6a6457854f85f7dffbd
Lucas <lucrecmen@gmail.com>
2023-04-17 19:06:13 -0300

    Revert "finished exercise 5.19 where things worked"
    This reverts commit 88b35f4dbb5d72911060c60b5628f556efa7cb3f.

--- src/com/curso/fjoo/sobrecarga/CadastroPessoa.java
+++ src/com/curso/fjoo/sobrecarga/CadastroPessoa.java
@@ -5,25 +5,13 @@ public class CadastroPessoa {
     public void cadastrar(Pessoa pessoa) {
 
         armazenar(pessoa.getNome(), pessoa.getIdade());
+
     }
-    public void cadastrar(String nome, int idade) {
-
-        armazenar(nome, idade);
-    }
-
-    public void cadastrar(String nome) {
-
-        armazenar(nome, 0);
-    }
-
-
-
 
 
 
     private void armazenar(String nome, int idade) {
-        // Salva isso no banco de dados.
-        System.out.println("Salvando: \"" + nome + "\" com " + idade + " anos.");
-    }
 
+        System.out.println("Salvando: \"" + nome + "\" com " + idade + "anos.");
+    }
 }
